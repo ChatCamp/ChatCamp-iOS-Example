613945c859e68010ed56d46765c0efe7b8233669
Saurabh Gupta <sg.jaipur@gmail.com>
2018-05-16 13:59:07 +0530

    replace attachments button, adds image preview, add zoom-in and zoom-out functionality for image preview, fixes one app crash

---
+++ ChatCamp Demo/ChatCamp Demo/View Controllers/ImagePreviewViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ImagePreviewViewController.swift
+//  ChatCamp Demo
+//
+//  Created by Saurabh Gupta on 16/05/18.
+//  Copyright © 2018 iFlyLabs Inc. All rights reserved.
+//
+
+import UIKit
+
+class ImagePreviewViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    var image: UIImage!
+    
+    override func viewDidLoad() {
+        scrollView.minimumZoomScale = 0.5
+        scrollView.maximumZoomScale = 3.0
+        scrollView.contentSize = imageView.frame.size
+        scrollView.delegate = self
+        imageView.image = image
+    }
+}
+
+extension ImagePreviewViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
+        
+    }
+}
