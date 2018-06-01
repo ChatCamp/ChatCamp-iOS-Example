d7ea37ae18687a79ea00821e9afd2bd2a4d5b174
Saurabh Gupta <sg.jaipur@gmail.com>
2018-06-01 18:35:38 +0530

    removes unused code

--- ChatCamp Demo/ChatCamp Demo/Extensions/UIImageView+ChatCamp.swift
+++ ChatCamp Demo/ChatCamp Demo/Extensions/UIImageView+ChatCamp.swift
@@ -20,29 +20,6 @@ let kGradientBotomOffset: HSVOffset = (hue: 0.025, saturation: -0.05, brightness
 
 extension UIImageView {
     
-    func roundCornersForAspectFit(radius: CGFloat) {
-        if let image = self.image {
-            
-            //calculate drawingRect
-            let boundsScale = self.bounds.size.width / self.bounds.size.height
-            let imageScale = image.size.width / image.size.height
-            
-            var drawingRect: CGRect = self.bounds
-            
-            if boundsScale > imageScale {
-                drawingRect.size.width =  drawingRect.size.height * imageScale
-                drawingRect.origin.x = (self.bounds.size.width - drawingRect.size.width) / 2
-            } else {
-                drawingRect.size.height = drawingRect.size.width / imageScale
-                drawingRect.origin.y = (self.bounds.size.height - drawingRect.size.height) / 2
-            }
-            let path = UIBezierPath(roundedRect: drawingRect, cornerRadius: radius)
-            let mask = CAShapeLayer()
-            mask.path = path.cgPath
-            self.layer.mask = mask
-        }
-    }
-    
     public func setImageForName(string: String, backgroundColor: UIColor? = nil, circular: Bool, textAttributes: [String: AnyObject]?, gradient: Bool = false) {
         
         setImageForName(string: string, backgroundColor: backgroundColor, circular: circular, textAttributes: textAttributes, gradient: gradient, gradientColors: nil)
